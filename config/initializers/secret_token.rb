df133d0db0f40f4ccf5e8a0baef5e802c1502313
carlos filho <ohlifsolrac@gmail.com>
2012-11-13 20:41:04 -0300

    aplicação de commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fasol::Application.config.secret_token = 'f8ae8e40685fb3eecfaa039787bd07213960119b443537c2d04117b6b293082ce10c4e337bd297cd93a7799a5dfc6c1c5d46a58d7a57cdd8da76a0e688f85d8e'
